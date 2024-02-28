3477dfd0a066a2a20f408fb437c5aa456b5a5cd4
Edgar Jesus Lopez Perez <159453307+edgarlpz13@users.noreply.github.com>
2024-02-28 15:02:40 +0000

    Actualizacion28/02/2024

--- biblioteca/libro.java
+++ biblioteca/libro.java
@@ -1,4 +1,4 @@
-package miPrincipal;
+package biblioteca;
 
 import biblioteca.libro;
 
@@ -10,4 +10,46 @@ public class libro {
         System.out.println(Math.sqrt(1.10000 * 1.10));
         System.out.println(Math.sqrt(1.1e200 * 1.1e200)); //este retorna un infinito positivo
         System.out.println(Math.sqrt(-1.1e200 * 1.1e200)); //este retorna un infinito negativo
-}   }
+        //Tipo de dato char
+        char car= 'A';
+        car='\101';
+        System.out.println(car);
+        car='\u0041';
+        System.out.println(car);
+        car = '\165';
+        System.out.println(car);
+        //tipo de dato boolean
+        boolean bandera;
+        bandera = true;
+        bandera = false;
+        if (bandera) System.out.println("Es verdadero");
+        //ejemplo de envoltorio
+        System.out.println(Integer.parseInt("123"));
+        System.out.println(Long.parseLong("165"));
+        System.out.println(Byte.parseByte("21"));
+        System.out.println(Short.parseShort("45"));
+        System.out.println(Double.parseDouble("1.5e8"));
+        System.out.println(Float.parseFloat("45.89"));
+        //LITERALES Y CONSTANTES
+        
+        //Para declarar constantes, anteponemos la palabra
+        //final en la declaracion y se utiliza un identificador
+        //mayuscula
+
+        final Double PI = 3.141592;
+
+        int octal = 015;
+        System.out.println(octal);
+
+        int decimal = 12;
+        System.out.println(decimal);
+
+        int hexadecimal = 0xA;
+        System.out.println(hexadecimal);
+
+
+
+
+
+}   }   
+
